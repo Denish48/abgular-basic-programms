523a30de489b62100238108dfaa2411862694173
denish4848 <denishsavaliya.elsner@gmail.com>
2022-03-03 12:13:55 +0530

    angbs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'moduleimpexp';
+  getalert(name: string) {
+    alert(name);
+  }
+  inp_value = '';
+  getinputvalue(data: any) {
+    // console.log(data)
+    this.inp_value = data;
+  }
+  disabledbox = true;
+  enablebox() {
+    this.disabledbox = false;
+  }
+  condition = 'red';
+  changecondition() {
+    this.condition = 'green';
+  }
+  holiday = '';
+  getholiday(name: string) {
+    // console.log("hey")
+    this.holiday = name.toLowerCase();
+  }
 }
